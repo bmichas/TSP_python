1609c45cf00aa8404425febc6dd0f1a2b98aaafb
Bartek <michasbartek@gmail.com>
2022-09-03 14:02:17 +0200

    astar

--- tsp_with_aStar/aStar.py
+++ tsp_with_aStar/aStar.py
@@ -1,5 +1,5 @@
-from typing_extensions import Self
 from classes.node import Node
+import numpy as np
 
 
 class aStar:
@@ -8,18 +8,22 @@ class aStar:
         self.startPoint = start_point
         self.currentPathCost = int  # int
         self.paths = dict  # dict
-        self.globalMin = int  # int
+        self.global_min = self._get_global_min()  # int
         self.currentPath = list  # list
 
-    def getGlobalMin(self):
+    def _get_global_min(self):
+        min_dist = float('inf')
         for city in self.cityMap:
-            city
+            lst = city.neighbors.values()
+            if min(lst) < min_dist:
+                min_dist = min(lst)
+        self.global_min = min_dist
+    
 
-    def heuristic(self):
+    def _heuristic(self):
         pass
 
-    def getPath(self):
-
+    def _get_path(self):
         pass
         # while len(self.currentPath) != len(self.cityMap) + 1:
         #     pass
